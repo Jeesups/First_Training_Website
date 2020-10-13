0ba12327db555e691ac7ed5dfedf2f2d5f79843d
Jeesup <mrpatiworld@gmail.com>
2020-10-13 11:07:53 +0200

    Starting working with JS

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/*
+console.log('Hello World!');
+var a = "Hello ";
+var b = "World";
+var c = a + b;
+console.log(c);
+console.log(a + b);
+
+let d = "Hello Wold ";
+let e = "World";
+e = "Heaven";
+const CONSTANT = 10;
+console.log(d + e);
+console.log(CONSTANT);
+*/
