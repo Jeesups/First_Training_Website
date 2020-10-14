2e07a31f3fb45eba9ef40fdf63ed1caf78aac138
Jeesup <mrpatiworld@gmail.com>
2020-10-14 12:04:51 +0200

    Continuation of JS part of the course

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,60 @@ const CONSTANT = 10;
 console.log(d + e);
 console.log(CONSTANT);
 */
+/* Data types */
+/* 
+var b = 2; // number
+var a = "Hello" //string
+var c = false; //boolean
+var d = 0.2; //float
+var e = null; //null 
+var f; //undefined
+var g = String(4);
+
+console.log(a,b,c,d,e,f, g + a);
+*/
+/* Math */
+/** 
+var a = 7;
+console.log(++a);
+console.log(a++);
+console.log(a);
+*/
+/** if statemets */
+/** 
+var a = 2;
+var b = 2;
+if(a == 1){
+    console.log("A is equal to 1");
+}
+else if (a === 2){
+    console.log("A is equal to 2 and it is Integer");
+}
+else if(a == 2){
+    console.log("A is equal to 2");
+}
+else{
+    console.log("A is neither 1 nor 2 ");
+}
+*/
+/** Arrays and their methods*/
+/** 
+let a = new Array();
+a = [1,2,3,4];
+let animal = ["Dog","Cat","Hamster","Fox"];
+
+animal.pop(); //deletes last value from array
+animal.shift(); // deletes fist value from array
+animal.unshift('cow'); //Adds value as first in the array
+animal.push('Horse'); //Appends value as last object in the array
+animal.splice(1,2, 'Wolf','Snake'); //Removes values from range, however, if you add third argument, then it will add record in place of deleted ones starting from first index in the argument
+
+let part = animal.slice(0,2);
+
+console.log(part);
+console.log(a);
+console.log(animal[0]);
+console.log(animal.length);
+console.log(animal.join('+'))
+*/
+/** Objects */
